d2140f2ff204939741fdce3c90d29122d7cd862b
Chirag Garg <38765776+DeVil2O@users.noreply.github.com>
2019-07-20 09:48:35 +0530

    Modification in Sir function and after You R on..
    modify the "You R on..".

--- botbody.py
+++ botbody.py
@@ -18,6 +18,12 @@ def copy_paste_of_file(file_name):
     dst_of_file=src_of_file + ".bak"                                           #making backup of a file 
     shutil.copy(src_of_file,dst_of_file)                                        #real copy making using shutil
     
+def yes_sir():                                                                  # Sir function for respect
+    stdout.write("YES SIR")                                                     # make all 3 functions of sir modified according to the question of the user.
+
+def no_sir():
+    stdout.write("NO SIR")
+    
 def sir():
     stdout.write("SIR")
 
@@ -25,8 +31,9 @@ def sir():
 #def make_sys_clear():
 #def game():
     
-stdout.write('\n\n\n'+"                                       You R on..")            #welcome print          
+stdout.write('\n\n\n'+(" "*20)+"You R on..")            #welcome print          
 
+stdout.write("Good "+)
 #system clear to be done
 
 stdout.write("What can I do for you?")                                                #taking command
