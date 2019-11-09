56ea70cffdbea4d9ed43d4708cf1bb95039eeec2
Chirag Garg <chrggrg2018@gmail.com>
2019-11-10 00:57:33 +0530

    Added the function to play the music

--- botbody.py
+++ botbody.py
@@ -3,8 +3,25 @@ Created on NOV 10 12:53 2019
 @author: devi
 """
 from title import *
+import os
+import subprocess, sys
 
 def starting_head():            #make a title file and import to it
     title_head()
 
-starting_head()
+def check_the_command(command):
+    command_list = ["play music"]
+    if command in command_list:
+        print("COMMAND ACCEPTED !!!")
+    else:
+        print("Please Enter A Valid Command")
+
+def process_the_command(command):
+    print("The Command Is Processing....")
+
+if __name__ == '__main__':
+    starting_head()
+    command = input("please enter the command : ".title())
+    opener ="open" if sys.platform == "darwin" else "xdg-open"
+    subprocess.call([opener,'/home/devi/Downloads/song1.mp3'])
+
