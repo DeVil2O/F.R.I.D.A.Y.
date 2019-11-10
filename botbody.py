9ce008fe6908ab4af7761e50ba45c5dd659eb28f
Chirag Garg <chrggrg2018@gmail.com>
2019-11-10 13:16:54 +0530

    Installed the heading to the body

--- botbody.py
+++ botbody.py
@@ -1,5 +1,10 @@
 """
-Created on Tue May 28 19:50:10 2019
+Created on NOV 10 12:53 2019
 @author: devi
 """
+from title import *
 
+def starting_head():            #make a title file and import to it
+    title_head()
+
+starting_head()
